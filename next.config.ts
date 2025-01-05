bd3227abe490f23d209ef8d2f8c75e12eba2203d
minhtien020900 <minhtien020900@gmail.com>
2025-01-05 17:28:41 +0700

    upload

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "export",
+
+  /* config options here */
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
