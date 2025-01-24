1513c2920c76e22f4f449f7aea5c818d875e95a6
[Lampart] Vo Minh Tien <minh_tien@lampart-vn.com>
2025-01-24 10:25:08 +0700

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,9 +4,9 @@ import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <AuthProvider>
+    // <AuthProvider>
       <Component {...pageProps} />
-    </AuthProvider>
+    // </AuthProvider>
   );
 }
 
